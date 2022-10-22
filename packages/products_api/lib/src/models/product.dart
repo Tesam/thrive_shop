e7ce674c5155d875703fa31074199955208788f7
Techi Samudio <mtsamudiog@gmail.com>
2022-10-22 15:34:36 -0300

    create product entity

---
+++ packages/products_api/lib/src/models/product.dart
@@ -0,0 +1,62 @@
+import 'package:equatable/equatable.dart';
+import 'package:products_api/src/models/category.dart';
+
+/// {@template product}
+/// A single product item.
+///
+/// Contains a [productId], [product], [category], [imageUrl], [isFavorite],
+/// and [favoriteDate].
+///
+/// [Product]s are immutable.
+/// {@endtemplate}
+class Product extends Equatable {
+  /// {@macro product}
+  const Product({
+    required this.productId,
+    required this.product,
+    required this.category,
+    required this.imageUrl,
+    this.isFavorite = false,
+    this.favoriteDate = '',
+  });
+
+  /// The unique identifier of the product.
+  ///
+  /// Cannot be empty.
+  final String productId;
+
+  /// The name of the product.
+  ///
+  /// Cannot be empty.
+  final String product;
+
+  /// The category data of the product.
+  ///
+  /// Cannot be empty.
+  final Category category;
+
+  /// The image url of the product.
+  ///
+  /// Cannot be empty.
+  final String imageUrl;
+
+  /// Whether the product is a favorite.
+  ///
+  /// Defaults to `false`.
+  final bool isFavorite;
+
+  /// The date when the product was set as favorite.
+  ///
+  /// Defaults to an empty string.
+  final String favoriteDate;
+
+  @override
+  List<Object?> get props => [
+        productId,
+        product,
+        category,
+        imageUrl,
+        isFavorite,
+        favoriteDate,
+      ];
+}
