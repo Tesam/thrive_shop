7cf4427445135b10c80b7cb07b7281e6b77f47b6
Techi Samudio <mtsamudiog@gmail.com>
2022-10-24 09:22:43 -0300

    create products repository

---
+++ packages/products_repository/lib/src/products_repository.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template products_repository}
+/// A repository that handles product requests.
+/// {@endtemplate}
+class ProductsRepository {
+  /// {@macro products_repository}
+  const ProductsRepository();
+}
