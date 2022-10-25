815bad38d66f55b75a220a119098dd765252cff6
Techi Samudio <mtsamudiog@gmail.com>
2022-10-24 22:28:49 -0300

    create backgrounds to manage add and delete favorite products

---
+++ lib/shopping_list/widgets/favorite_swipe_background.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:thrive_shop/color_schemes.g.dart';
+
+class FavoriteSwipeBackground extends StatelessWidget {
+  const FavoriteSwipeBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: AppColors.lightColorScheme.secondary,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Row(
+          children: const[
+            SizedBox(
+              width: 20,
+            ),
+            Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ),
+            Text(
+              ' Favorite',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
