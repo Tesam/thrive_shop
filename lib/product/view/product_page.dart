2f1b4190799b3eac288da008fe788103cb829efd
Techi Samudio <mtsamudiog@gmail.com>
2022-10-25 18:58:24 -0300

    feat(product_page): add a switch to manage product and category forms

--- lib/product/view/product_page.dart
+++ lib/product/view/product_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:thrive_shop/color_schemes.g.dart';
 import 'package:thrive_shop/product/view/category_form.dart';
+import 'package:thrive_shop/product/view/product_form.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  bool _isProduct = true;
+  String _title = 'Product';
+
+  void changeMode(bool value) {
+    if (value) {
+      setState(() {
+        _isProduct = true;
+        _title = 'Create Product';
+      });
+    } else {
+      setState(() {
+        _isProduct = false;
+        _title = 'Create Category';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +34,26 @@ class ProductPage extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
       child: Column(
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                _title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                ),
+              ),
+              Switch(
+                value: _isProduct,
+                onChanged: (value) => changeMode(value),
+                activeColor: AppColors.lightColorScheme.primary,
+              )
+            ],
+          ),
+          const SizedBox(height: 20,),
           Expanded(
-            child: CategoryForm(),
+            child: _isProduct ? const ProductForm() : const CategoryForm(),
           ),
         ],
       ),
