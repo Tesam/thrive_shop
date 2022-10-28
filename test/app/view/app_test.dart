8eca50ba50c3a0fb59ba3acde8c2e2dfae07d21a
Techi Samudio <mtsamudiog@gmail.com>
2022-10-27 22:51:49 -0300

    remove unused code

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -5,15 +5,3 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:thrive_shop/app/app.dart';
-import 'package:thrive_shop/counter/counter.dart';
-
-void main() {
- /* group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });*/
-}
