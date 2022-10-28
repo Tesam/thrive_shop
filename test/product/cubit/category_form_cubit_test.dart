9854683683260155a1e0e0696825604509f8b15e
Techi Samudio <mtsamudiog@gmail.com>
2022-10-28 09:06:47 -0300

    test(category_form_cubit_test.dart): test de onColorChange() function

--- test/product/cubit/category_form_cubit_test.dart
+++ test/product/cubit/category_form_cubit_test.dart
@@ -5,6 +5,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:products_repository/products_repository.dart';
 import 'package:thrive_shop/product/cubit/category_form_cubit.dart';
 import 'package:thrive_shop/product/models/category_input.dart';
+import 'package:thrive_shop/product/models/color_input.dart';
 
 class ProductsRepositoryMock extends Mock implements ProductsRepository {}
 
@@ -12,8 +13,6 @@ void main() {
   group('CategoryFormCubit', () {
     late ProductsRepositoryMock productsRepositoryMock;
     late CategoryFormCubit categoryFormCubit;
-
-    const categoryFormState = CategoryFormState();
 
     setUp(() {
       productsRepositoryMock = ProductsRepositoryMock();
@@ -27,20 +26,33 @@ void main() {
     test(
         'The initial state is CategoryFormState'
         ' (category: , color:0xFF006783, state:FormzStatus.pure)', () {
-      expect(categoryFormCubit.state, categoryFormState);
+      expect(categoryFormCubit.state, const CategoryFormState());
     });
 
+    blocTest(
+      'the cubit should emit a CategoryFormState(category:Category1,'
+      ' color:0xFF006783, state:FormzStatus.valid) when onCategoryChange()'
+      ' is called',
+      build: () => categoryFormCubit,
+      act: (cubit) => cubit.onCategoryChanged('Category 1'),
+      expect: () => [
+        const CategoryFormState(
+          category: CategoryInput.dirty('Category 1'),
+          status: FormzStatus.valid,
+        )
+      ],
+    );
 
     blocTest(
-        'the cubit should emit a CategoryFormState(category:Category1,'
-        ' color:0xFF006783, state:FormzStatus.valid) when onCategoryChange()'
-        ' is called',
-        build: () => categoryFormCubit,
-        act: (cubit) => cubit.onCategoryChanged('Category 1'),
-        expect: () => [
-          const CategoryFormState(
-              category: CategoryInput.dirty('Category 1'),
-              status: FormzStatus.valid,)
-        ],);
+      'the cubit should emit a CategoryFormState(category: ,'
+          ' color:0xFF006711, state:FormzStatus.valid) when onColorChange()'
+          ' is called',
+      build: () => categoryFormCubit,
+      act: (cubit) => cubit.onColorChanged(0xFF006711),
+      expect: () => [
+        const CategoryFormState(
+          color: ColorInput.dirty(0xFF006711),
+          status: FormzStatus.valid,)
+      ],);
   });
 }
