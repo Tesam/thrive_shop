a1aafd2c4d5a792b981923d94f6f1a5d3835d790
Techi Samudio <mtsamudiog@gmail.com>
2022-11-14 11:22:08 -0300

    test fixes

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -4,3 +4,8 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('app_view_app_test_dart', () {});
+}
